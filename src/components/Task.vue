319b2af33e55adeee85d65e82da57789c3fea27f
Ugesh ROOPCHAND <uroopchand@Prodigious.mu>
2022-09-29 17:05:36 +0400

    double click toggle task reminder

---
+++ src/components/Task.vue
@@ -0,0 +1,56 @@
+<template>
+    <!-- #region -->
+     <!-- use a tenary v bind for class add a reminder class if it true -->
+     <!--<div :class="[task.reminder ? 'reminder': '', 'task']">-->
+    <!-- #endregion -->
+    <div 
+     @dblclick="$emit('toggle-reminder', task.id)"
+     :class="[task.reminder ? 'reminder': '', 'task']">
+        <h3>{{ task.text }}
+        <i @click="$emit('delete-task',id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{ task.day}}</p>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+    // methods: {
+    //     onDelete(id){
+    //         //#region
+    //         //How to Emit Custom Events in Vue
+    //         //Many Vue patterns involve passing data from a parent 
+    //         //component to its children using props. But what if we need a child to affect its parent?
+    //         //https://learnvue.co/tutorials/vue-emit-guide
+    //             //$emit 
+    //         //#endregion
+    //       this.$emit('delete-task',id)
+    //     }
+    // }
+}
+</script>
+
+<style scope>
+    .fas {
+      color: red;
+    }
+    .task {
+      background: #f4f4f4;
+      margin: 5px;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+    .task.reminder {
+      border-left: 5px solid green;
+    }
+    .task h3 {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <!--Catch emit from Task child vuel leve 3 . 
+        You pass emit her @delete-task="$emit('delete-task',task.id)"-->
+        <Task  @toggle-reminder = "$emit('toggle-reminder', task.id)" 
+        @delete-task="$emit('delete-task',task.id)" :task="task" />
+    </div>
+</template>
+<script>
+import Task from './Task.vue';
+
+export default {
+   name: 'Tasks',
+   props: {
+    tasks: Array
+   },
+   components: {
+    Task
+   },
+
+   emits: ['delete-task','toggle-reminder']
+}
+</script>
